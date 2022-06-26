5fefe8f0ebf7fa958e382e1f7dccba39b7972902
AbnocChinwads <rnemo@hotmail.co.uk>
2022-06-26 11:22:02 +0000

    fixed path to game.html, added canvas test circle

--- game/views.py
+++ game/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def game(request):
     """ A view to return the index page """
-    return render(request, 'home/game.html')
+    return render(request, 'game/game.html')
